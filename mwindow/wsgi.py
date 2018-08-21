01ffe89a54c1c5b5f7dbc3b369e9fd8d37159f8c
thanh07 <mob_thanh@yahoo.com>
2018-08-21 09:55:25 +0700

    new updated master

--- mwindow/wsgi.py
+++ mwindow/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mwindow3 project.
+WSGI config for mwindow project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Color, Profile, Aluminium
 # Register your models here.
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Color, ColorAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Profile, ProfileAdmin)
+
+class AluminiumAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code','slug','description', 'leng','change','created','updated']
+    list_filter = ['code','created', 'updated']
+    list_editable = ['change',]
+    prepopulated_fields = {'slug': ('code',)}
+admin.site.register(Aluminium, AluminiumAdmin)
